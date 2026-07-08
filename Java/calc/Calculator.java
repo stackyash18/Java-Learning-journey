caa932fc2bdb6cd215d0ded63703d04a1e420da3
yash tomar <tomary880@gmail.com>
2026-07-08 23:44:16 +0530

    formatted the code"

--- Java/calc/Calculator.java
+++ Java/calc/Calculator.java
@@ -1,64 +1,55 @@
 package calc;
 
-class SimpleCalculator
-{
-   protected int proInt = 4;
-    int defInt = 5;
-  public void addNum(int a, int b)
-  {
-  System.out.println("Your Result is: " + (a+b));
+class SimpleCalculator {
+  protected int proInt = 4;
+  int defInt = 5;
+
+  public void addNum(int a, int b) {
+    System.out.println("Your Result is: " + (a + b));
   }
 
-  public void minusNum(int a, int b)
-  {
-  System.out.println("Your Result is: " + (a-b));
+  public void minusNum(int a, int b) {
+    System.out.println("Your Result is: " + (a - b));
   }
-  public void multNum(int a, int b)
-  {
-    System.out.println("Your Result is: " + (a*b));
+
+  public void multNum(int a, int b) {
+    System.out.println("Your Result is: " + (a * b));
   }
-  public void divNum(int a, int b)
-  {
-  System.out.println("Your Result is: " + (a/b));
+
+  public void divNum(int a, int b) {
+    System.out.println("Your Result is: " + (a / b));
   }
 }
 
-class ScCalculator
-{
-  public void sumOfSin(int a, int b)
-  {
-    System.out.println("The Value of sin(a+b) is:  " + Math.sin(a+b));
+class ScCalculator {
+  public void sumOfSin(int a, int b) {
+    System.out.println("The Value of sin(a+b) is:  " + Math.sin(a + b));
   }
 
-   public void diffOfSin(int a, int b)
-  {
-    System.out.println("The Value of sin(a-b) is:  " + Math.sin(a-b));
-  }
-  
-   public void sumOfCos(int a, int b)
-  {
-    System.out.println("The Value of cos(a+b) is:  " + Math.cos(a+b));
+  public void diffOfSin(int a, int b) {
+    System.out.println("The Value of sin(a-b) is:  " + Math.sin(a - b));
   }
 
-   public void diffOfCos(int a, int b)
-  {
-    System.out.println("The Value of cos(a-b) is:  " + Math.cos(a-b));
+  public void sumOfCos(int a, int b) {
+    System.out.println("The Value of cos(a+b) is:  " + Math.cos(a + b));
+  }
+
+  public void diffOfCos(int a, int b) {
+    System.out.println("The Value of cos(a-b) is:  " + Math.cos(a - b));
   }
 }
 
-class HybCalc
-{
-  public void calcInterest(int noOfYears, float rate, int principalAmount)
-  {
-  float interest = (rate/100)*principalAmount;
-  float totalAmount = principalAmount + interest*noOfYears;
-  System.out.println("The Total Amount is:  " + totalAmount);  
+class HybCalc {
+  public void calcInterest(int noOfYears, float rate, int principalAmount) {
+    float interest = (rate / 100) * principalAmount;
+    float totalAmount = principalAmount + interest * noOfYears;
+    System.out.println("The Total Amount is:  " + totalAmount);
+  }
 }
-}
+
 public class Calculator {
-  public static void main(String args[])
-  {
-   
+  public static void main(String args[]) {
+
     System.out.println("This is Calculator");
   }
 }
--- Java/calc/UseOfSubclassInAccessModifier.java
+++ Java/calc/UseOfSubclassInAccessModifier.java
@@ -1,16 +1,14 @@
- package calc;
+package calc;
 
- class c1 extends  SimpleCalculator
- {
- void proSol()
- {
- System.out.println(proInt);
- System.out.println(defInt);
- }
- }
+class c1 extends SimpleCalculator {
+  void proSol() {
+    System.out.println(proInt);
+    System.out.println(defInt);
+  }
+}
+
 public class UseOfSubclassInAccessModifier {
-  public static void main(String args[])
-  {
+  public static void main(String args[]) {
     System.out.println("I am Main Method.");
     c1 work = new c1();
     System.out.println(work.defInt);
