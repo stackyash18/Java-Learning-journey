e3aa934337e48501b6b4551e8ed580e32b97a2b6
yash tomar <tomary880@gmail.com>
2026-07-10 01:26:53 +0530

    Adding the basic challenges in java language

---
+++ Java/Practise Exercise/ProductOfFloatingNumber.java
@@ -0,0 +1,12 @@
+public class ProductOfFloatingNumber {
+  public static void main(String args[])
+  {
+    float a = 3.02f;
+    float b = 6.25f;
+
+    float result = a*b;
+
+    System.out.println(result);
+  }
+  
+}
