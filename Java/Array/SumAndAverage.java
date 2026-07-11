b15154de1a1089666882d98e9c1b110432d618d8
yash tomar <tomary880@gmail.com>
2026-07-11 21:13:12 +0530

    Added the file for calculating sum in 2D array

--- Java/Array/SumAndAverage.java
+++ Java/Array/SumAndAverage.java
@@ -3,6 +3,28 @@ package Array;
 public class SumAndAverage {
   public static void main(String args[])
   {
-    
+    System.out.println("Calculating sum and average of all the element in 2D array.");
+    int arr[][] = ArrayUtility.input2DArray();
+
+    int sum = SumOf2D(arr);
+
+
+    System.out.print("Sum of all the element in 2D Array: " + sum);
+
+  }
+
+  public static int SumOf2D(int arr[][])
+  {
+    int sum = 0;
+
+    for(int i=0; i<arr.length; i++)
+    {
+      for(int j=0; j<arr[i].length; j++)
+
+        {
+          sum+=arr[i][j];
+        }
+    }
+    return sum;
   }
 }
