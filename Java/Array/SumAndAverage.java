cd0fd2c3d3ce7e9dd3bb25080d5388d5b5c6f080
yash tomar <tomary880@gmail.com>
2026-07-11 22:38:06 +0530

    G

--- Java/Array/SumAndAverage.java
+++ Java/Array/SumAndAverage.java
@@ -1,45 +1,37 @@
 package Array;
 
 public class SumAndAverage {
-  public static void main(String args[])
-  {
+  public static void main(String args[]) {
     System.out.println("Calculating sum and average of all the element in 2D array.");
     int arr[][] = ArrayUtility.input2DArray();
 
     int sum = sumOf2D(arr);
-
 
     System.out.println("Sum of all the element in 2D Array: " + sum);
 
     double avg = average(arr);
     System.out.println("Average of all the elements in 2D Array: " + avg);
 
-    
-
   }
 
-  public static double average(int arr[][])
-  {
-    if(arr.length==0)
-    {
+  public static double average(int arr[][]) {
+    if (arr.length == 0) {
       return 0;
     }
     int rows = arr.length;
     int columns = arr[0].length;
-    return sumOf2D(arr)/(rows*columns);
+    return sumOf2D(arr) / (rows * columns);
   }
 
-  public static int sumOf2D(int arr[][])
-  {
+  public static int sumOf2D(int arr[][]) {
     int sum = 0;
 
-    for(int i=0; i<arr.length; i++)
-    {
-      for(int j=0; j<arr[i].length; j++)
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++)
 
-        {
-          sum+=arr[i][j];
-        }
+      {
+        sum += arr[i][j];
+      }
     }
     return sum;
   }
