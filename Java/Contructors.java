6c6fd80fcd3b53b17fe7007fe7a61ca69b7faf6b
yash tomar <tomary880@gmail.com>
2026-07-14 00:34:53 +0530

    Editted the constructor file  again

--- Java/Contructors.java
+++ Java/Contructors.java
@@ -1,35 +1,27 @@
-class MyCollegeData {
-  private int id;
-  private String name;
+class Constructors
+{
+  public class Car
+  {
+   String color;
+   float price;
+   int fuelInLitre;
+   int MaxSpeedInKM;
 
-  public MyCollegeData() {
-    id = 14;
-    name = "Yash Tomar";
+   Car()
+   {
+    color = "black";
+    price = 400225.212f;
+    MaxSpeedInKM = 120;
+    fuelInLitre = 5;
+   }
+   
+   public class start
+   {
+    
+   }
   }
+  public static void main(String args[])
+  {
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String n) {
-    this.name = n;
-  }
-
-  public void setId(int i) {
-    this.id = i;
-  }
-
-  public int getId() {
-    return id;
-  }
-}
-
-public class Contructors {
-  public static void main(String[] args) {
-    MyCollegeData Yash = new MyCollegeData();
-    // Yash.setName("Yash TOmar");
-    // Yash.setId(3291);
-    System.out.println(Yash.getId());
-    System.out.println(Yash.getName());
   }
 }
