7696f9b885c786c56bc969153ca9cfeaf0ca39c0
yash tomar <tomary880@gmail.com>
2026-07-16 14:32:00 +0530

    Added the file for foreach loop statement

---
+++ Java/ForEachLoop.java
@@ -0,0 +1,14 @@
+public class ForEachLoop {
+  public static void main(String args[])
+  {
+    String arr[] = new String[]
+    {
+      "Mango","Apple","Grapes","Papaya"," Watermellon","Strawberry"
+    };
+
+    for(String name : arr)
+    {
+      System.out.println(name);
+    }
+  }
+}
