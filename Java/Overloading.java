7833a09698aa02972f91a6de8a84094d7a0148c1
yash tomar <tomary880@gmail.com>
2026-07-30 00:36:49 +0530

    implemented the concept of method overloading

---
+++ Java/Overloading.java
@@ -0,0 +1,24 @@
+public class Overloading {
+  public int sum(int a, int b)
+  {
+    return a + b;
+  }
+
+  public int sum(int a, int b, int c)
+  {
+    return a + b + c;
+  }
+
+  public int sum(int a, int b, int c, int d)
+  {
+    return a + b + c + d;
+  }
+
+  public static void main(String args[])
+  {
+    Overloading over = new Overloading();
+    System.out.println(over.sum(45,145));
+    System.out.println(over.sum(44,44,17));
+    System.out.println(over.sum(25,52,35,24));
+  }
+}
