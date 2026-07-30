e47969bfa18c46ad2aa9890c684668ee7d57f0ec
yash tomar <tomary880@gmail.com>
2026-07-30 12:10:42 +0530

    Added the sum method in code using method overriding

---
+++ Java/Vehicle/Main.java
@@ -0,0 +1,33 @@
+package Vehicle;
+
+public class Main {
+  
+  static class Vehicle
+  {
+    public void service()
+    {
+      System.out.println("The vehicle has been serviced.");
+    }
+
+  }
+
+  static class Car extends Vehicle
+  {
+    @Override
+    public void service()
+    {
+      System.out.println("The car service has beem done.");
+      super.service();
+    }
+  }
+
+  public static void main(String args[])
+  {
+    Vehicle veh = new Vehicle();
+    veh.service();
+
+    Car car = new Car();
+    car.service();
+
+  }
+}
