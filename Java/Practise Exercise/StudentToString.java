10f5c1cbfdf82ecac972cc93702babf393c4839d
yash tomar <tomary880@gmail.com>
2026-08-01 22:23:55 +0530

    A

--- Java/Practise Exercise/StudentToString.java
+++ Java/Practise Exercise/StudentToString.java
@@ -3,8 +3,8 @@ public class StudentToString {
   int age;
   String rollNumber;
   String house;
-  public StudentToString(String name, int age, String rollNumber, String house)
-  {
+
+  public StudentToString(String name, int age, String rollNumber, String house) {
     this.name = name;
     this.age = age;
     this.rollNumber = rollNumber;
@@ -12,13 +12,12 @@ public class StudentToString {
   }
 
   @Override
-  public String toString()
-  {
-    return "Student details:{name:"+name+", age:"+age+" +, roll number:"+rollNumber+", house:,"+house+"}";
+  public String toString() {
+    return "Student details:{name:" + name + ", age:" + age + " +, roll number:" + rollNumber + ", house:," + house
+        + "}";
   }
 
-  public static void main(String args[])
-  {
+  public static void main(String args[]) {
     StudentToString stu = new StudentToString("Yash", 18, "7675439", "04");
     System.out.println(stu);
   }
--- Java/Vehicle/Main.java
+++ Java/Vehicle/Main.java
@@ -1,28 +1,23 @@
 package vehicle;
 
 public class Main {
-  
-  static class Vehicle
-  {
-    public void service()
-    {
+
+  static class Vehicle {
+    public void service() {
       System.out.println("The vehicle has been serviced.");
     }
 
   }
 
-  static class Car extends Vehicle
-  {
+  static class Car extends Vehicle {
     @Override
-    public void service()
-    {
+    public void service() {
       System.out.println("The car service has beem done.");
       super.service();
     }
   }
 
-  public static void main(String args[])
-  {
+  public static void main(String args[]) {
     Vehicle veh = new Vehicle();
     veh.service();
 
