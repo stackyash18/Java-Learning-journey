4ed1f85ae6282ce5f02747b114df876884984236
yash tomar <tomary880@gmail.com>
2026-08-13 12:57:49 +0530

    added the ListInterface file

---
+++ Java/Collections Library/ListInterface.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListInterface {
+  public static void main(String args[])
+  {
+    List<Integer> list = new ArrayList<Integer>();
+    list.add(40);
+  }
+}
