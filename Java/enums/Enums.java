1970580069c8f1862b332890f59abcb639a0f86e
yash tomar <tomary880@gmail.com>
2026-08-16 11:24:34 +0530

    Added the enum files

---
+++ Java/enums/Enums.java
@@ -0,0 +1,9 @@
+package enums;
+public class Enums {
+  public static void main(String args[])
+  {
+     TrafficLight color = TrafficLight.RED; 
+     System.out.println(color);
+     
+  }
+}
---
+++ Java/enums/TrafficLight.java
@@ -0,0 +1,12 @@
+package enums;
+
+public enum TrafficLight {
+  RED("Stop."), GREEN("Go."), YELLOW("Caution");
+
+  private final String action;
+
+  TrafficLight(String action)
+  {
+    this.action = action;
+  }
+}
