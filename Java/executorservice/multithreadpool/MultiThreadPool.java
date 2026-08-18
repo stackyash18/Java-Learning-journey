9d39539c3119feececc8e01fde9e3a9f81089aa4
yash tomar <tomary880@gmail.com>
2026-08-19 01:32:52 +0530

    Added the multi threading in executor service file

--- Java/executorservice/multithreadpool/MultiThreadPool.java
+++ Java/executorservice/multithreadpool/MultiThreadPool.java
@@ -1,8 +1,22 @@
 package executorservice.multithreadpool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import multithreading.runnable.PrintTask;
+
 public class MultiThreadPool {
   public static void main(String args[])
   {
-    
+    ExecutorService service = Executors.newFixedThreadPool(3);
+    PrintTask task1 = new PrintTask('#');
+    PrintTask task2 = new PrintTask('*');
+    PrintTask task3 = new PrintTask('$');
+
+      service.submit(task1);
+      service.submit(task2);
+      service.submit(task3);
+
+      service.shutdown();
   }
 }
